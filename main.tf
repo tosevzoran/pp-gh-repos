406a307ae9a3636e0ba209e624ed4283b6b1364d
zoki <tosevzoran@gmail.com>
2024-12-27 21:08:53 +0100

    feat(branch-protection): add branch protection for the main branch (#2)
    * feat(branch-protection): add branch protection for the main branch
    
    * feat(branch-protection): lock branch and require reviewers
    
    * feat(branch-protection): remove required reviewers

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "github_repository" "self" {
   description            = each.value.description
   delete_branch_on_merge = true
 }
+
+resource "github_branch_protection" "self" {
+  for_each = local.repos
+
+  pattern                 = "main"
+  repository_id           = each.key
+  required_linear_history = true
+  allows_deletions        = false
+  lock_branch             = true
+}
