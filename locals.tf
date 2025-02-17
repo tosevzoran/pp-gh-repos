1f9f57eb4d8f3edf19b88ed10c8f3c05e1bfdf09
zoki <tosevzoran@gmail.com>
2025-02-17 21:11:18 +0100

    chore: remove repos

--- locals.tf
+++ locals.tf
@@ -10,20 +10,20 @@ locals {
       gitignore_template = "Terraform"
       visibility         = "public"
     }
-    "pp-gh-do-droplets" = {
-      description        = "Terraform repository for managing DigitalOcean droplets"
-      gitignore_template = "Terraform"
-      visibility         = "public"
-    }
-    "pp-gh-astro-blog" = {
-      description        = "Astro blog repo"
-      gitignore_template = ""
-      visibility         = "public"
-    }
-    "pp-gh-astro-blog2" = {
-      description        = "Astro blog repo 2"
-      gitignore_template = ""
-      visibility         = "public"
-    }
+    # "pp-gh-do-droplets" = {
+    #   description        = "Terraform repository for managing DigitalOcean droplets"
+    #   gitignore_template = "Terraform"
+    #   visibility         = "public"
+    # }
+    # "pp-gh-astro-blog" = {
+    #   description        = "Astro blog repo"
+    #   gitignore_template = ""
+    #   visibility         = "public"
+    # }
+    # "pp-gh-astro-blog2" = {
+    #   description        = "Astro blog repo 2"
+    #   gitignore_template = ""
+    #   visibility         = "public"
+    # }
   }
 }
